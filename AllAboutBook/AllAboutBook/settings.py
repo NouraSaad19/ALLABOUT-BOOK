60ade5f7164d72e67e7e9692ddb39aad53e6c924
noura <noura@gmail.com>
2022-06-14 15:43:26 +0300

    project update

--- AllAboutBook/AllAboutBook/settings.py
+++ AllAboutBook/AllAboutBook/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'AllAboutBook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AllAboutBook_finalProject',
+        'USER': 'noura98',
+        'PASSWORD': 'n14191419',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
